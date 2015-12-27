542b7f7fc5be77bb46945cc10b8ab468f6f51fbd
Chandler <chongweizhang@hotmail.com>
2015-12-28 00:48:26 +0800

    新建一个抽象框架，未来用于更改CAA

---
+++ ModelExperiment/Models/Modes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public abstract class Material
+    {
+     
+    }
+
+    public abstract class Process
+    {
+        public List<Equipment> EquipmentNeeded;
+        public List<Material> MaterialNeeded;
+
+ 
+    }
+    public abstract class WorkCell
+    {
+        
+ 
+    }
+    public abstract class Equipment
+    {
+        public List<Equipment> OwnEquitments;
+        public void AddEquitment(Equipment equip)
+        {
+            OwnEquitments.Add(equip);
+ 
+        }
+        virtual public bool RegiestEquitmentToWorkCell();
+ 
+    }
+    public interface Control
+    {
+ 
+    }
+
+    
+
+}
